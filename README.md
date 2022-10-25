151ff409949f180b76d00398dff5bc9ddc1535e5
Adam Slay <aslay@mesonet.org>
2022-10-25 09:54:49 -0500

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Oklahoma Hourly Forecast Map
-This is a simple Forecast Map using weather data from Weather.gov to display hourly forecast data in Oklahoma.
-My primary goals with this project were to utilize asynchronous programing and Docker. 
+This is a simple Forecast Map using station info from [Meteostat](https://dev.meteostat.net/python/daily.html#example) and weather data from [Weather.gov](https://weather-gov.github.io/api/general-faqs) to display hourly forecast data in Oklahoma.
+My primary goals with this project were to utilize asynchronous programing and Docker containerization. 
 
 ## Running the program
 To build the Docker image, clone the repo then execute the following command while in the project directory:
--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -1,24 +1,29 @@
-import src
+import unittest
+from unittest import mock
 from src.forecast_api import Forecast
 from src.forecast_plot import Plot
-from meteostat import Stations
 
 
-def test_Forecast() -> None:
-    loc = [35, 94]
-    fore = Forecast(loc)
-    assert fore.lat == loc[0]
-    assert fore.lon == loc[1]
-    pass
+class Test_Forecast(unittest.TestCase):
+    def test_init(self) -> None:
+        loc = [35, 94]
+        fore = Forecast(loc)
+        assert fore.lat == loc[0]
+        assert fore.lon == loc[1]
+        pass
+
+    # @mock.patch()
+    # def test_get_json(self):
+    #    pass
 
 
-def test_Plot():
-    t_path = "2022-10-24--11-59"
-    t_print = "Monday October 24, 2022 11:59 AM"
-    plot = Plot(t_print, t_path)
-    assert plot.t_path == t_path
-    assert plot.t_print == t_print
-    pass
+class Test_Plot(unittest.TestCase):
+    def test_init(self) -> None:
+        t_path = "2022-10-24--11-59"
+        t_print = "Monday October 24, 2022 11:59 AM"
+        plot = Plot(t_print, t_path)
+        assert plot.t_path == t_path
+        assert plot.t_print == t_print
 
 
 def test_create_df():
